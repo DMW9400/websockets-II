b6dcc55fb43af90ecfbc5b0708eb697dfbd7c2ab
github username <github email address>
2018-06-19 14:36:27 -0400

    configured messages controller

--- api-websockets/app/controllers/conversations_controller.rb
+++ api-websockets/app/controllers/conversations_controller.rb
@@ -8,12 +8,18 @@ class ConversationsController < ApplicationController
   def create
     conversation = Conversation.new(conversation_params)
     if conversation.save
-      serialized_data = ActiveModelSerializers::Adapter::JSON.new(
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
         ConversationSerializer.new(conversation)
       ).serializable_hash
       ActionCable.server.broadcast 'conversations_channel', serialized_data
       head :ok
     end
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:title)
   end
 
 
