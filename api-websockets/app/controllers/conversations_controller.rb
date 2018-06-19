2bc5a0bdd818d736cf13d4278ee91e5a9fb6490f
github username <github email address>
2018-06-19 14:30:27 -0400

    created controllers, configured conversations controller

---
+++ api-websockets/app/controllers/conversations_controller.rb
@@ -0,0 +1,20 @@
+class ConversationsController < ApplicationController
+  def index
+    conversations = Conversation.all
+    render json: conversations
+    # allows JS to pull in the serialized data and display as a single-page-application without need for refresh
+  end
+
+  def create
+    conversation = Conversation.new(conversation_params)
+    if conversation.save
+      serialized_data = ActiveModelSerializers::Adapter::JSON.new(
+        ConversationSerializer.new(conversation)
+      ).serializable_hash
+      ActionCable.server.broadcast 'conversations_channel', serialized_data
+      head :ok
+    end
+  end
+
+
+end
