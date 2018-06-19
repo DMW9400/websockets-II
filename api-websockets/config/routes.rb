3cfd878c08abf4a640ffbfd611c33eae1e96995e
github username <github email address>
2018-06-19 13:29:46 -0400

    mounted ActionCable in routes, added redis gem

--- api-websockets/config/routes.rb
+++ api-websockets/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :conversations, only: [:index, :create]
+  resources :messages, only: [:create]
+
+  mount ActionCable.server => '/cable'
+  # since ActionCable is its own protocol, typical 'Get'/'Post'/etc. routes are not relevant - this mounting creates an endpoint called 'cable' (by convention) that serves as the endpoint where the client instantiates the websocket connection with the server 
 end
