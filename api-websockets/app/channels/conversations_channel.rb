7529a7c10ff6cb45628f5b3d30cd1776673d0b0d
github username <github email address>
2018-06-19 13:48:53 -0400

    added streams for conversations and messages channels, added serializers"

--- api-websockets/app/channels/conversations_channel.rb
+++ api-websockets/app/channels/conversations_channel.rb
@@ -1,6 +1,7 @@
 class ConversationsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "conversations_channel"
+    # stream_from expects a string as an argument - in this case the hardcoded conversations channel
   end
 
   def unsubscribed
--- api-websockets/app/channels/messages_channel.rb
+++ api-websockets/app/channels/messages_channel.rb
@@ -1,6 +1,8 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    conversation = Conversation.find(params[:conversation])
+    stream_for conversation
+    # stream_for expects an object- in this case an instance of the conversation class that is found via the conversation params
   end
 
   def unsubscribed
