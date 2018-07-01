ad9fb5e5917ba9a43fb7e20d08b9ffb910918a2c
github username <github email address>
2018-07-01 11:04:33 -0400

    added action cable to index

--- frontend-websockets/src/index.js
+++ frontend-websockets/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ActionCableProvider url={API_WS_ROOT} />
+  <App />, document.getElementById('root')
+   </ActionCableProvider>,
+);
 registerServiceWorker();
