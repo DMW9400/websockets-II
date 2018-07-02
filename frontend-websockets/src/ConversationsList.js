60661d2daacfc3c3e7169fa41829753f0397387f
github username <github email address>
2018-07-01 22:51:27 -0400

    imports for conversationslist component

--- frontend-websockets/src/ConversationsList.js
+++ frontend-websockets/src/ConversationsList.js
@@ -1,1 +1,6 @@
 import React from 'react'
+import { ActionCable } from 'react-actioncable-provider';
+import NewConversationForm from './NewConversationForm';
+import { API_ROOT } from '../constants';
+import MessagesArea from './MessagesArea';
+import Cable from './Cable';
